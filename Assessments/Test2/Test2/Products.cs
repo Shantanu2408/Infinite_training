66e78670a7f5a96d44dbf0da93b742ca5f462e67
Shantanu Singh <shantanusi@infinite.com>
2024-03-06 10:51:57 +0530

    Second

---
+++ Assessments/Test2/Test2/Products.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Product
+    {
+        int Produc_tId;
+        string Product_Name;
+        int price;
+
+        public static void SortedListFunction()
+        {
+            SortedList sr = new SortedList();  
+
+
+            sr.Add(190, "Pencil");
+            sr.Add(80, "Pen");
+            sr.Add(70, "Apple");
+            sr.Add(85, "Orange");
+            sr.Add(70, "Mixer");
+            sr.Add(190, "Medicine");
+            sr.Add(230, "Dry Fruits");
+            sr.Add(84, "Umbrella");
+            sr.Add(90, "Lichi");
+            sr.Add(48, "Papaya");
+            Console.WriteLine("------Sorted List-------");
+            foreach (DictionaryEntry st in sr)
+            {
+                Console.WriteLine("" + st.Key + " " + st.Value);
+                Console.WriteLine();
+            }
+
+        }
+    }
+}
---
+++ Assessments/Test2/Test2/Student.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Student
+{
+    abstract class Student
+    {
+        public string Name { get; set; }
+        public int StudentId { get; set; }
+        public double Grade { get; set; }
+
+        public abstract bool IsPassed(double grade);
+    }
+
+    class Undergraduate : Student
+    {
+        public override bool IsPassed(double grade)
+        {
+            return grade > 70.0;
+        }
+    }
+
+    class Graduate : Student
+    {
+        public override bool IsPassed(double grade)
+        {
+            return grade > 80.0;
+        }
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("----- Abstract class called  Student with  Name, StudentId, Grade as members -----");
+
+            
+            Undergraduate undergradStudent = new Undergraduate
+            {
+                Name = "Shantanu Singh",
+                StudentId = 1034313,
+                Grade = 81.0
+            };
+
+
+            Console.WriteLine($"Undergraduate Student: {undergradStudent.Name}, ID: {undergradStudent.StudentId}, Grade: {undergradStudent.Grade}");
+            Console.WriteLine($"Is Passed: {undergradStudent.IsPassed(undergradStudent.Grade)}\n");
+            
+            Graduate gradStudent = new Graduate
+            {
+                Name = "Prashant Singh",
+                StudentId = 9567898,
+                Grade = 83.0
+            };
+
+            Console.WriteLine($"Graduate Student: {gradStudent.Name}, ID: {gradStudent.StudentId}, Grade: {gradStudent.Grade}");
+            Console.WriteLine($"Is Passed: {gradStudent.IsPassed(gradStudent.Grade)}");
+            Console.ReadLine();
+        }
+    }
+}
